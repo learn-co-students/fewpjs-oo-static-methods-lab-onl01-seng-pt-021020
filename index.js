c8b1cc3901d0f0c2fd2154dcf15f74843c645fd1
tyler jones <rteyl1690@gmail.com>
2020-09-09 03:50:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-_' ]/g, '')
+  }
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(' ')
+      .map((w,i) => {
+         if (i===0 || !exceptions.includes(w)){
+          return this.capitalize(w)
+        }else {
+          return w
+        }
+       })
+      .join(' ')
+    }
 }

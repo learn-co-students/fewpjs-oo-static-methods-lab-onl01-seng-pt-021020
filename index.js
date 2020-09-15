536ba2175e11a1039c45727bc57c1e834c081842
StevenDRiggs <sfriggs21@gmail.com>
2020-09-14 19:52:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]/g, '')
+  }
+
+  static titleize(str) {
+    const words = str.split(/\s+/)
+    const excludedWords = [
+      'the',
+      'a',
+      'an',
+      'but',
+      'of',
+      'and',
+      'for',
+      'at',
+      'by',
+      'from'
+    ]
+    
+    for (let i=0; i < words.length; i++) {
+      if (!excludedWords.includes(words[i]) || i === 0) {
+        words[i] = this.capitalize(words[i])
+      }
+    }
+
+    return words.join(' ')
+  }
 }
